45dc69c99d6ce3e7459f7f63b990db672107b543
michael <michael@kneesystems.com>
2017-05-31 09:15:40 +1000

    Add self host windows service

---
+++ OwinWebApi/SelfHostWindowsService/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SelfHostWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour, minute;
+            Console.WriteLine("Please specify the time (as format of HH:MM) you want to run the task");
+            Console.Write("For example, you can put 20:30, which means 8:30PM:");
+            var input = Console.ReadLine();
+            TryToGetHourMinute(input, out hour, out minute);
+
+            var business = true;//your business
+            var timer = new ScheduleTimer(hour, minute, () => business);
+            Console.WriteLine($"The task runs at {hour}:{minute} everyday");
+            do
+            {
+                Console.WriteLine("\nAt anytime you can type in 'Stop' to stop the task or new time to adjust task run time from the console.\n");
+                input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+                if (input.Equals("stop", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    timer.Stop();
+                    break;
+                }
+                TryToGetHourMinute(input, out hour, out minute);
+                timer.StartAt(hour, minute);
+                Console.WriteLine($"The task run at {hour}:{minute} everyday");
+            } while (true);
+        }
+
+        private static void TryToGetHourMinute(string timeSet, out int hour, out int minute)
+        {
+            while (!ParseTimeToHourMinute(timeSet, out hour, out minute))
+            {
+                Console.Write("Please try to set the right time as format of HH:MM where HH is in [0-23] and MM is in [0-59]:");
+                timeSet = Console.ReadLine();
+            }
+        }
+
+        private static bool ParseTimeToHourMinute(string timeSet, out int hour, out int minute)
+        {
+            try
+            {
+                var hourMinutes = timeSet.Split(':');
+                if (!hourMinutes.Length.Equals(2))
+                    throw new ArgumentException("The time format is not correct.");
+
+                hour = int.Parse(hourMinutes[0]);
+                if (hour < 0 || hour > 23)
+                    throw new ArgumentException($"The hour is {hour} which is out of range");
+
+                minute = int.Parse(hourMinutes[1]);
+                if (minute < 0 || minute > 59)
+                    throw new ArgumentException($"The minute is {minute} which is out of range");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                hour = minute = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ OwinWebApi/SelfHostWindowsService/ScheduleTimer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Timers;
+
+namespace SelfHostWindowsService
+{
+    public class ScheduleTimer
+    {
+        private Timer _timer;
+
+        private readonly Func<bool> _func;
+
+        private DateTime _nowTime;
+
+        private DateTime _scheduledTime;
+
+        public ScheduleTimer(int hour, int minute, Func<bool> func)
+        {
+            _func = func;
+            _nowTime = DateTime.Now;
+            _scheduledTime = new DateTime(_nowTime.Year, _nowTime.Month, _nowTime.Day, hour, minute, 0, 0);
+            ScheduleTask();
+        }
+
+        public void ScheduleTask()
+        {
+            _nowTime = DateTime.Now;
+            if (_nowTime > _scheduledTime)
+            {
+                _scheduledTime = _scheduledTime.AddDays(1);
+            }
+
+            Console.WriteLine($"Task next run is at {_scheduledTime}");
+
+            double tickTime = (double)(_scheduledTime - _nowTime).TotalMilliseconds;
+            _timer = new Timer(tickTime);
+            _timer.Elapsed += TimerElapsed;
+            _timer.Start();
+        }
+
+        public void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine($"Task run is at {DateTime.Now}");
+            _timer.Stop();
+            bool result = _func();
+            Console.WriteLine($"Task run finished. Succeeded:{result} \n");
+            ScheduleTask();
+        }
+
+        public void Stop() => _timer.Stop();
+
+        public void Start() => _timer.Start();
+
+        public void StartAt(int hour, int minute)
+        {
+            _nowTime = DateTime.Now;
+            _scheduledTime = new DateTime(_nowTime.Year, _nowTime.Month, _nowTime.Day, hour, minute, 0, 0);
+            ScheduleTask();
+        }
+    }
+}
