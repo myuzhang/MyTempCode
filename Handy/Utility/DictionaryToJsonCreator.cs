7f850ff3883cfaeb949c05623a71afcb57ffa876
Michael Zhang <michael.zhang@coringroup.com>
2017-07-10 10:06:06 +1000

    Add handy sln

---
+++ Handy/Utility/DictionaryToJsonCreator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Utility
+{
+    public class DictionaryToJsonCreator
+    {
+        private readonly StringBuilder _builder;
+
+        private readonly Dictionary<string, string> _dictionary;
+
+        public DictionaryToJsonCreator()
+        {
+            _builder = new StringBuilder();
+            _builder.Append("{}");
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        public static DictionaryToJsonCreator GetBuilder() => new DictionaryToJsonCreator();
+
+        public string this[string key]
+        {
+            get
+            {
+                if (_dictionary.ContainsKey(key.Trim()))
+                    return _dictionary[key.Trim()];
+
+                return null;
+            }
+        }
+
+        public DictionaryToJsonCreator Add(Dictionary<string, string> dictionary)
+        {
+            foreach (var keyValue in dictionary)
+            {
+                _dictionary[keyValue.Key.Trim()] = keyValue.Value.Trim();
+            }
+            return this;
+        }
+
+        public DictionaryToJsonCreator Add(string key, string value)
+        {
+            _dictionary[key.Trim()] = value.Trim();
+            return this;
+        }
+
+        public string GetString()
+        {
+            foreach (var keyValue in _dictionary)
+            {
+                _builder.Insert(_builder.Length - 1, $",\"{keyValue.Key}\":\"{keyValue.Value}\"");
+            }
+            return RemoveFirst(_builder.ToString(), ",");
+        }
+
+        public T GetObject<T>() where T : class => JsonConvert.DeserializeObject<T>(GetString());
+
+        public string GetStringFromJsonFile(string jsonFile)
+        {
+            string jsonString;
+            using (StreamReader r = new StreamReader(jsonFile))
+            {
+                jsonString = r.ReadToEnd();
+            }
+            return jsonString;
+        }
+
+        public T GetObjectFromJsonFile<T>(string jsonFile) where T : class => JsonConvert.DeserializeObject<T>(GetStringFromJsonFile(jsonFile));
+
+        private string RemoveFirst(string source, string remove)
+        {
+            int index = source.IndexOf(remove, StringComparison.Ordinal);
+            return (index < 0)
+                ? source
+                : source.Remove(index, remove.Length);
+        }
+    }
+}
---
+++ Handy/Utility/ExcelReader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LinqToExcel;
+using LinqToExcel.Attributes;
+
+namespace Utility
+{
+    public class ExcelReader
+    {
+        private readonly ExcelQueryFactory _book;
+
+        private readonly string _excelFile;
+
+        public ExcelReader(string excelFile)
+        {
+            _book = new ExcelQueryFactory(excelFile)
+            {
+                //DatabaseEngine = LinqToExcel.Domain.DatabaseEngine.Ace,
+                TrimSpaces = LinqToExcel.Query.TrimSpacesType.Both,
+                //UsePersistentConnection = true,
+                ReadOnly = true
+            };
+            _excelFile = excelFile;
+        }
+
+        public static ExcelReader GetExcelReader(string excelFile) => new ExcelReader(excelFile);
+
+        public List<Row> GetAllRowsBySheet(string sheet = null)
+        {
+            IQueryable<Row> all = QueryRowsBySheet(sheet);
+            return all.ToList();
+        }
+
+        public IQueryable<Row> QueryRowsBySheet(string sheet = null)
+        {
+            return from a in _book.Worksheet(sheet ?? Path.GetFileNameWithoutExtension(_excelFile)) select a;
+        }
+
+        public IQueryable<T> QueryRowsBySheet<T>(string sheet = null) where T:class
+        {
+            return from a in _book.Worksheet<T>(sheet ?? Path.GetFileNameWithoutExtension(_excelFile)) select a;
+        }
+    }
+
+    /// <summary>
+    /// To serialize to a class.
+    /// </summary>
+    public class SerializeExample
+    {
+        // the column name
+        [ExcelColumn("ColumnName")]
+        public string SurgeonName { get; set; }
+
+        // if and only if no column name, column number being used
+        [ExcelColumn("F20")]
+        public int PreferF { get; set; }
+
+        // no attribute, no serializing
+        public bool DefaultSurgeon { get; set; }
+    }
+}
